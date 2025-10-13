f0cf120d1fcef3b17a7148573ed69d1bf14ffd17
Amit Paudel <amitpaudel789@gmail.com>
2025-10-13 18:05:32 +0545

    file upload and download start

---
+++ File Upload and Download/controller/authController.js
@@ -0,0 +1,163 @@
+const { check, validationResult } = require('express-validator');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    pageTitle: 'Login',
+    currentPage: 'Login',
+    isLoggedIn: false,
+    errors: [],
+    oldInput: { email: '' },
+    user: {},
+  });
+};
+exports.postLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(422).render('auth/login', {
+      pageTitle: 'Login',
+      currentPage: 'login',
+      isLoggedIn: false,
+      errors: ["User doesn't exist"],
+      oldInput: { email },
+      user: {},
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(422).render('auth/login', {
+      pageTitle: 'Login',
+      currentPage: 'login',
+      isLoggedIn: false,
+      errors: ['Invalid Password'],
+      oldInput: { email },
+      user: {},
+    });
+  }
+
+  // res.cookie('isLoggedIn', true);
+  req.session.isLoggedIn = true;
+  req.session.user = user;
+  await req.session.save();
+  res.redirect('/');
+};
+
+exports.postLogout = (req, res, next) => {
+  // res.cookie('isLoggedIn', false);
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+};
+
+exports.getSignUp = (req, res, next) => {
+  res.render('auth/signup', {
+    pageTitle: 'SignUp',
+    currentPage: 'SignUp',
+    isLoggedIn: false,
+    errors: [],
+    oldInput: { firstName: '', lastName: '', email: '', userType: '' },
+    user: {},
+  });
+};
+
+exports.postSignUp = [
+  check('firstName')
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage('First Name should be atleast 2 characters long')
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage('First Name should contain only alphabets'),
+
+  check('lastName')
+    .matches(/^[A-Za-z\s]*$/)
+    .withMessage('Last should contain only alphabets'),
+
+  check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email')
+    .normalizeEmail(),
+
+  check('password')
+    .isLength({ min: 8 })
+    .withMessage('Password should contain atleast 8 letters')
+    .matches(/[A-Z]/)
+    .withMessage('Password should contain atleast one uppercase letter')
+    .matches(/[a-z]/)
+    .withMessage('Password should contain atleast one lowercase letter')
+    .matches(/[0-9]/)
+    .withMessage('Password should contain atleast one number')
+    .matches(/[!@#$%^&*(){}]/)
+    .withMessage('Password should contain atleast one special character')
+    .trim(),
+
+  check('confirmPassword')
+    .trim()
+    .custom((value, { req }) => {
+      if (value != req.body.password) {
+        throw new Error('Password do not match');
+      }
+      return true;
+    }),
+
+  check('userType')
+    .notEmpty()
+    .withMessage('Please select a user type')
+    .isIn(['guest', 'host'])
+    .withMessage('Invalid user type'),
+
+  check('terms')
+    .notEmpty()
+    .withMessage('Please agreee with terms and conditions')
+    .custom((value, { req }) => {
+      if (value !== 'on') {
+        throw new Error('Please accept terms and condition');
+      }
+      return true;
+    }),
+
+  (req, res, next) => {
+    const { firstName, lastName, password, email, userType } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      //true=no error
+      //false=error
+      //So, !errors.isEmpty() means If there are errors present
+      return res.status(422).render('auth/signup', {
+        pageTitle: 'SignUp',
+        currentPage: 'signup',
+        isLoggedIn: false,
+        errors: errors.array().map((err) => err.msg),
+        oldInput: { firstName, lastName, email, userType },
+        user: {},
+      });
+    }
+
+    bcrypt.hash(password, 12).then((hashedPassword) => {
+      const user = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        userType,
+      });
+      user
+        .save()
+        .then(() => {
+          res.redirect('/login');
+        })
+        .catch((err) => {
+          return res.status(422).render('auth/signup', {
+            pageTitle: 'SignUp',
+            currentPage: 'signup',
+            isLoggedIn: false,
+            errors: [err.message],
+            oldInput: { firstName, lastName, email, userType },
+            user: {},
+          });
+        });
+    });
+  },
+];
---
+++ File Upload and Download/controller/storeController.js
@@ -0,0 +1,108 @@
+const Favourite = require('../models/favourite');
+const Home = require('../models/home');
+
+exports.getIndex = (req, res, next) => {
+  console.log('Session Value ', req.session);
+  Home.find().then((registeredHomes) => {
+    console.log(registeredHomes);
+    res.render('store/index', {
+      registeredHomes: registeredHomes,
+      pageTitle: 'airbnb ',
+      currentPage: 'index',
+      isLoggedIn: req.isLoggedIn,
+      user: req.session.user,
+    });
+  });
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.find().then((registeredHomes) => {
+    res.render('store/home-list', {
+      registeredHomes: registeredHomes,
+      pageTitle: 'airbnb Home',
+      currentPage: 'Home-List',
+      isLoggedIn: req.isLoggedIn,
+      user: req.session.user,
+    });
+  });
+};
+exports.getBookings = (req, res, next) => {
+  res.render('store/booking', {
+    pageTitle: 'My Booking',
+    currentPage: 'Bookings',
+    isLoggedIn: req.isLoggedIn,
+    user: req.session.user,
+  });
+};
+
+exports.getFavouriteList = (req, res, next) => {
+  Favourite.find().then((favourites) => {
+    favourites = favourites.map((fav) => fav.houseId.toString());
+
+    Home.find().then((registeredHomes) => {
+      const favouriteHomes = registeredHomes.filter((home) =>
+        favourites.includes(home._id.toString())
+      );
+      res.render('store/favourite-list', {
+        favouriteHomes: favouriteHomes,
+        pageTitle: 'Favourite Home',
+        currentPage: 'Favourite',
+        isLoggedIn: req.isLoggedIn,
+        user: req.session.user,
+      });
+    });
+  });
+};
+
+exports.postAddFavourite = (req, res, next) => {
+  const homeId = req.body.id;
+  Favourite.findOne({ houseId: homeId })
+    .then((fav) => {
+      if (fav) {
+        console.log('Already marked as favourite');
+      } else {
+        fav = new Favourite({ houseId: homeId });
+        fav.save().then((result) => {
+          console.log('Fav added', result);
+        });
+      }
+
+      res.redirect('/favourite');
+    })
+    .catch((error) => {
+      console.log('Error while marking favourites', error);
+    });
+};
+
+exports.getHomeDetails = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Home.findById(homeId).then((home) => {
+    if (!home) {
+      console.log('home not found');
+      res.redirect('/homes');
+    } else {
+      res.render('store/home-detail', {
+        home: home,
+        pageTitle: 'Home-detail',
+        currentPage: 'Home',
+        isLoggedIn: req.isLoggedIn,
+        user: req.session.user,
+      });
+    }
+  });
+};
+
+exports.deleteFav = (req, res, next) => {
+  const favId = req.params.favId;
+  console.log(favId);
+  Favourite.findOneAndDelete({ houseId: favId })
+    .then((result) => {
+      console.log('Fav removed', result);
+    })
+    .catch((err) => {
+      console.log('Error while removing favourite', err);
+    })
+    .finally(() => {
+      res.redirect('/favourite');
+    });
+};
